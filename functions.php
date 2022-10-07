33ce6ea625cfd0e5fc11d5ba743c6ea6abea0b9e
chansamson <christiansamson0102@gmail.com>
2022-10-07 22:45:57 +0800

    Homepage
    Section:
    Banner
    Healthcare

--- functions.php
+++ functions.php
@@ -174,7 +174,7 @@ add_action( 'widgets_init', 'medcare_widgets_init' );
  * Enqueue scripts and styles.
  */
 function medcare_scripts() {
-	wp_enqueue_style( 'medcare-style', get_stylesheet_uri(), array(), _S_VERSION );
+	wp_enqueue_style( 'medcare-style', get_stylesheet_uri(), array(), time() );
 	//Bootstrap link - css
 	wp_enqueue_style( 'bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css' );
 	
@@ -184,7 +184,9 @@ function medcare_scripts() {
 	//Bootstrap link - script
 	wp_enqueue_script( 'bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.min.js' );
 
-	
+	//Font Awesome
+	wp_enqueue_style( 'load-fa', 'https://use.fontawesome.com/releases/v5.5.0/css/all.css' );
+
 	wp_style_add_data( 'medcare-style', 'rtl', 'replace' );
 
 	wp_enqueue_script( 'medcare-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
